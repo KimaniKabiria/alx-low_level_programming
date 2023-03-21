d52249a3db7fde8cbfe446acc48cdd8d3d3f9555
Kimani Kabiria <kabiriakimani@gmail.com>
2023-03-21 14:27:07 +0300

    Fix 007

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
+ * main - print Holberton, followed by new line
+ * Return: 0
  */
 
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar('u');
+	_putchar('r');
+	_putchar('i');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
