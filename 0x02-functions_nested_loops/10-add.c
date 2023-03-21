79fe8f0a06216ba95f3c4c5b7e79790140057a19
Kimani Kabiria <kabiriakimani@gmail.com>
2023-03-21 10:48:28 +0300

    Finished Mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: two integer arguments
+ * @b: two integer arguments
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+    // int sum;
+    // sum = a + b;
+    
+    // _putchar(sum);
+    // return (0);
+    
+	return (a + b);
+}
