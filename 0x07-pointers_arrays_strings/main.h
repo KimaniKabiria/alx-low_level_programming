e89786396ed9c23ede859f54d75607daa5104587
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-03 04:48:58 -0700

    7-print_chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,65 +3,50 @@
 
 /**
  * _putchar - puts character to standard output
- * @c: character to put to standard output
  */
 
 int _putchar(char c);
 
 /**
  * _memset - fill n bytes of memory with a constant value
- * @s: pointer to memory area
- * @b: constant value
- * @n: number of bytes to fill
- * Return: memory area
  */
 
 char *_memset(char *s, char b, unsigned int n);
 
 /**
  * _memcpy - copy n bytes of memory from source to destination
- * @dest: destination
- * @src: source
- * @n: number of bytes
- * Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 /**
  * _strchr - locates a character in a string
- * @s: string to search
- * @c: target character
- * Return: pointer to c
  */
 
 char *_strchr(char *s, char c);
 
 /**
  * _strspn - return length of string that matches values consistently
- * @s: string to search
- * @accept: target matches
- * Return: number of bytes consecutively matched
  */
 
 unsigned int _strspn(char *s, char *accept);
 
 /**
  * _strpbrk - return pointer to byte in s that matches a byte in accept
- * @s: string to search
- * @accept: target matches
- * Return: pointer to index of string at first occurence
  */
 
 char *_strpbrk(char *s, char *accept);
 
 /**
- * _strstr - locate and return pointer to first occurence of substring
- * @haystack: string to search
- * @needle: target substring to search for
- * Return: pointer to index of string at first occurence of whole substring
+ * _strstr - locate and return pointer to first occurence of sbstring
  */
 
 char *_strstr(char *haystack, char *needle);
+
+/**
+ * print_chessboard - print chessboard given set 2D array
+ */
+
+void print_chessboard(char (*a)[8]);
 
 #endif
