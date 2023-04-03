3cfff821d996f130da64ca8eae43db45323c843a
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-03 04:56:11 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -49,4 +49,10 @@ char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
 
+/**
+ * print_diagsums - print sums of diagonals in matrix
+ */
+
+void print_diagsums(int *a, int size)
+
 #endif
