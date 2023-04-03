1b8d5b01b4b09b84f0d7c45cecf171aa9304776a
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-03 03:24:42 -0700

    Betty Linter Fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,15 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
-{	
+{
+
 	int size = n;
+
 	/* Only Positive sizes */
 	if (size > 0)
 	{
 		int i;
+
 		for (i = 0; i < size; i++)
 		{
 			s[i] = b;
