b15979bfd137058905b52aee2e5221d98f468436
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-05 04:28:57 -0700

    Fixes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,30 +3,30 @@
 /**
  * find_root - find square root of n, starting from the smallest possible, 0
  * @n: n
- * @root: test this root
+ * @r: is root?
  * Return: natural square root, or -1 if not natural root
  */
 
-int find_root(int n, int root)
+int find_root(int n, int r)
 {
-	if (root * root > n)
+	if (r * r > n)
 		return (-1);
 
-	if (root * root == n)
-		return (root);
+	if (r * r == n)
+		return (r);
 
-	return (find_root(n, root + 1));
+	return (find_root(n, r + 1));
 }
 
 /**
  * _sqrt_recursion - find natural square root of n
- * @n: n
+ * @n: number
  * Return: natural square root, or -1 if not natural root
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	if (n < 0) /* is natural? */
 		return (-1);
 
 	return (find_root(n, 0));
