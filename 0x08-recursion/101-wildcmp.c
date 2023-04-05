5b6f43bd30c6a4aea235fbb437af3ba8eb6ef389
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-05 05:34:41 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,10 +12,10 @@ int matcher(char *s1, char *s2, char *s_hold)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+        if (*s1 == '\0' && *s2 == '*')
+                return (matcher(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
-	if (*s1 == '\0' && *s2 == '*')
-		return (matcher(s1, s2 + 1, s2 + 1));
 	if (*s2 == '*')
 		return (matcher(s1, s2 + 1, s2 + 1));
 	if (*s1 == *s2)
