ee29b898c13f30efc5d92d2d5207cd51cd1d0e8d
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-10 04:26:06 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - print sum if all arguments given are numbers
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < argc)
+	{
+		if (!(argv[i] >= '0' && argv[i] <= '9'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
