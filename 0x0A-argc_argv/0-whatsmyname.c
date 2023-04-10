ae81bcb13a6302055688d07f5ec3f0ae5e7e8011
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-10 03:41:21 -0700

    fix task o

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - it will print a name, followed by a new line.
@@ -10,8 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 0>
-			printf("%s\n", argv[0]);
+	if (argc > 0>)
+		printf("%s\n", argv[0]);
+
 	return (0);
 }
 
