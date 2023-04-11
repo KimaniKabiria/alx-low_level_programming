a51a9f0b431a71cda027d52e70457b913cf27d9e
Alvin Kimani <kabiriakimani@gmail.com>
2023-04-11 14:35:04 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,49 +1,93 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <ctype.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * strtow - splits string to words
+ * number_w - get number of words
  * @str: string
- * Return: pointer to array of strings
+ * Return: number of words
+ */
+
+int number_w(char *str)
+{
+	int i, num = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			for (; str[i] != ' ' && str[i] != '\0'; i++)
+				str++;
+			num++;
+		}
+	}
+	return (num);
+}
+
+/**
+ * freer - frees everything
+ * @str: pointer to free
+ * @i: counter
+ */
+
+void freer(char **str, int i)
+{
+	for (; i > 0;)
+		free(str[--i]);
+	free(str);
+}
+
+/**
+ * strtow - splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
  */
 
 char **strtow(char *str)
 {
-	int c = 0, i = 0, j = 0;
-	int length = strlen(str);
-	char **w = malloc((length + 1) * sizeof(char *));
+	int total_w = 0, i = 0, j = 0, length = 0;
+	char **w, *found_w;
 
-	if (str == NULL || *str == '\0')
+	if (str == 0 || *str == 0)
 		return (NULL);
-
-	if (w == NULL)
+	total_w = number_w(str);
+	if (total_w == 0)
 		return (NULL);
-
-	for (i = 0; i < length && str[i]; i++)
+	w = malloc((total_w + 1) * sizeof(char *));
+	if (w == 0)
+		return (NULL);
+	for (i = 0; *str != '\0' &&  i < total_w; i++)
 	{
-		if (!isspace(str[i]))
+		if (*str == ' ')
+			str++;
+		else
 		{
-			c++;
-			j = 1;
-			while (j < length && !isspace(str[j]))
-				j++;
-			w[c - 1] = malloc(j - i + 1);
-			if (w[c - 1] == NULL)
+			found_w = str;
+			for (; *str != ' ' && *str != '\0';)
 			{
-				while (--c >= 0)
-					free(w[c]);
-				free(w);
+				length++;
+				str++;
+			}
+			w[i] = malloc((length + 1) * sizeof(char));
+			if (w[i] == 0)
+			{
+				freer(w, i);
 				return (NULL);
 			}
-			strncpy(w[c - 1], &str[i], j - i);
-			w[c - 1][j - i] = '\0';
-			i = j;
+			while (*found_w != ' ' && *found_w != '\0')
+			{
+				words[i][j] = *found_w;
+				found_w++;
+				j++;
+			}
+			words[i][j] = '\0';
+			j = 0;
+			length = 0;
+			str++;
 		}
 	}
-	w[c] = NULL;
 	return (w);
 }
 
