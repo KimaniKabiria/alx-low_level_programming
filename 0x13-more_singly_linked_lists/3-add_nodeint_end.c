c66b2ba3ec67fe9563502914a34922f72ed79717
Alvin Kimani <kabiriakimani@gmail.com>
2023-05-02 11:58:58 -0700

    Fix wron variable reference

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	listint_t *n_node, *tmp_node;
 
-	new_node = malloc(sizeof(listint_t));
+	n_node = malloc(sizeof(listint_t));
 	if (n_node == NULL)
 		return (NULL);
 
