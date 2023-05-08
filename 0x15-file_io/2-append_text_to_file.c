908cbfaf6e8653bf3c1a8f8b149c7507adde8f68
Alvin Kimani <kabiriakimani@gmail.com>
2023-05-08 15:14:59 -0700

    Fix len

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int n_wrote;
+	size_t len = 0;
 
 	if (!filename)
 		return (-1);
@@ -27,8 +28,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	n_wrote = write(fd, text_content, _strlen(text_content));
-	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	while (text_content[len] != '\0')
+		len++;
+
+	n_wrote = write(fd, text_content, len);
+	if (n_wrote == -1)
 	{
 		close(fd);
 		return (-1);
